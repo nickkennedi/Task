c5e8938ec9e9f1f31b78b8ada72f0ec1ac7d4509
Nick Kennedy <nickconta10@gmail.com>
2024-01-01 20:22:33 -0300

    firebase add auth recover password

--- app/src/main/java/com/nickkennedi/task/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/nickkennedi/task/ui/auth/RecoverAccountFragment.kt
@@ -5,21 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nickkennedi.task.R
+import android.widget.Toast
+import androidx.core.view.isVisible
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+import com.nickkennedi.task.databinding.FragmentRecoverAccountBinding
 
 class RecoverAccountFragment : Fragment() {
 
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_recover_account,
-            container,
-            false
-        )
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        initClick()
+    }
+
+    private fun initClick() {
+
+        binding.btnSend.setOnClickListener { validateData() }
+
+    }
+
+    private fun validateData(){
+        val email = binding.edtEmail.text.toString().trim()
+
+
+        if(email.isNotEmpty()){
+            binding.progressBar.isVisible = true
+            recoverAccountUser(email)
+        }else{
+            Toast.makeText(requireContext(), "Informe seu e-mail.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun recoverAccountUser(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(requireContext(), "Link enviado com sucesso", Toast.LENGTH_SHORT).show()
+                }
+                binding.progressBar.isVisible = false
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
